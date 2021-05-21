d293b7b991731db72a448d38de1b37fbc4eb5e93
kruppdawg <80928882+kruppdawg@users.noreply.github.com>
2021-05-21 11:28:13 -0400

    Add files via upload

---
+++ RPGProject/src/PlayerCharacter.java
@@ -0,0 +1,10 @@
+
+public class PlayerCharacter {
+	final String PLAYER_NAME;
+	final String PLAYER_CHARACTER_NAME;
+	boolean playerCharacterIsHuman;
+	int playerIntellect;
+	double playerStrength;
+	float playerEngineering;
+	
+}
