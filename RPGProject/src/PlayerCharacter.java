44662340dbf0f70a534012959f91ae64ee302f14
kruppdawg <80928882+kruppdawg@users.noreply.github.com>
2021-05-21 12:26:00 -0400

    Add files via upload

---
+++ RPGProject/src/PlayerCharacter.java
@@ -0,0 +1,9 @@
+//Andrew A. Krupp
+public class PlayerCharacter {
+	boolean playerCharacterIsHuman;
+	int playerIntellect;
+	double playerStrength;
+	float playerEngineering;
+	
+	// Here I have created variables for the player's character as they play the game. 
+}
