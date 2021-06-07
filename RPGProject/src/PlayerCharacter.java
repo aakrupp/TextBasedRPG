72f6cd93a21fa028ac89a7fc66e71123f4a32797
kruppdawg <80928882+kruppdawg@users.noreply.github.com>
2021-06-07 10:48:19 -0400

    Delete RPGProject directory

--- RPGProject/src/PlayerCharacter.java
+++
@@ -1,9 +0,0 @@
-//Andrew A. Krupp
-public class PlayerCharacter {
-	boolean playerCharacterIsHuman;
-	int playerIntellect;
-	double playerStrength;
-	float playerEngineering;
-	
-	// Here I have created variables for the player's character as they play the game. 
-}
